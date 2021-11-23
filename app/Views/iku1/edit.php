d719205a58e4860763d23054d4ab5dc60b7c8d01
Adam Mukti <adammukti08@gmail.com>
2021-11-23 16:03:24 +0700

    fix status validasi in index and edit iku1

--- app/Views/iku1/edit.php
+++ app/Views/iku1/edit.php
@@ -1,123 +1,130 @@
-<?php echo view('layout/header')?>
-<?php echo view('layout/navbar')?>
-<?php echo view('layout/sidebar')?>
+<?php echo view('layout/header') ?>
+<?php echo view('layout/navbar') ?>
+<?php echo view('layout/sidebar') ?>
+
+<div class="main-content">
+  <section class="section">
 
 
-<div class="col-12">
-            <div class="card">
-                  <div class="card-header">
-                    <h4>Edit Table Iku 1</h4>
-                  </div>
-                <form action="<?php echo base_url('iku1/update/'.$iku_1['id']); ?>" method="post">
-                  <div class="card-body">
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Jenis</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input name="jenis" type="text"class="form-control" value="<?php echo $iku_1['jenis']; ?>">
-                          <!-- <option  value="Bapenda">Bapenda</option>
-                          <option  value="Dinkes">Dinkes</option>
-                          <option  value="Disdik">Disdik</option> -->
-                        </select>
-                        
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="nama" value="<?php echo $iku_1['nama']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">NIM</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="nim" value="<?php echo $iku_1['nim']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Jenjang</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="jenjang" value="<?php echo $iku_1['jenjang']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Prodi</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="prodi" value="<?php echo $iku_1['prodi']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Fakultas</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="fakultas" value="<?php echo $iku_1['fakultas']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">No. Ijazah</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="no_ijazah" value="<?php echo $iku_1['no_ijazah']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Masa Tunggu</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="masa_tunggu" value="<?php echo $iku_1['masa_tunggu']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama Perusahaan</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="nama_perusahaan" value="<?php echo $iku_1['nama_perusahaan']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Lokasi Perusahaan</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="lokasi_perusahaan" value="<?php echo $iku_1['lokasi_perusahaan']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Pendapatan</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="pendapatan" value="<?php echo $iku_1['pendapatan']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Lanjut Studi</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="lanjut_studi" value="<?php echo $iku_1['lanjut_studi']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tautan Bukti</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input type="text" class="form-control" name="tautan_bukti" value="<?php echo $iku_1['tautan_bukti']; ?>">
-                      </div>
-                    </div>
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status Validasi</label>
-                      <div class="col-sm-12 col-md-7">
-                        <input name="status_validasi" type="text"class="form-control" value="<?php echo $iku_1['status_validasi']; ?>">
-                            <option value="1">Ya</option>
-                            <option value="0">Tidak</option>
-                        </select>
-                      </div>
-                    </div>
-                    
-                    <div class="form-group row mb-4">
-                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
-                      <div class="col-sm-12 col-md-7">
-                        <button class="btn btn-primary">Update</button>
-                        <a href="<?= base_url('iku1')?>" class="btn btn-danger">Back</a>
-                      </div>
-                    </div>
-                  </div>
-</form>
-                
-                
+    <div class="card">
+      <div class="card-header">
+        <h4>Edit Data Iku 1</h4>
+      </div>
+      <form action="<?php echo base_url('iku1/update/' . $iku_1['id']); ?>" method="post">
+        <div class="card-body">
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Jenis</label>
+            <div class="col-sm-12 col-md-7">
+              <select name="jenis" type="text" class="form-control" value="<?php echo $iku_1['jenis']; ?>">
+                <option value="Mendapat Pekerjaan" <?php if ($iku_1['jenis'] == 'Mendapatkan Pekerjaan') echo 'selected' ?>>Mendapat Pekerjaan</option>
+                <option value="Melanjutkan Studi" <?php if ($iku_1['jenis'] == 'Melanjutkan Studi') echo 'selected' ?>>Melanjutkan Studi</option>
+                <option value="Menjadi Wiraswasta" <?php if ($iku_1['jenis'] == 'Menjadi Wiraswasta') echo 'selected' ?>>Menjadi Wiraswasta</option>
+              </select>
+
             </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="nama" value="<?php echo $iku_1['nama']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">NIM</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="nim" value="<?php echo $iku_1['nim']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Jenjang</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="jenjang" value="<?php echo $iku_1['jenjang']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Prodi</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="prodi" value="<?php echo $iku_1['prodi']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Fakultas</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="fakultas" value="<?php echo $iku_1['fakultas']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">No. Ijazah</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="no_ijazah" value="<?php echo $iku_1['no_ijazah']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Masa Tunggu</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="masa_tunggu" value="<?php echo $iku_1['masa_tunggu']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nama Perusahaan</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="nama_perusahaan" value="<?php echo $iku_1['nama_perusahaan']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Lokasi Perusahaan</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="lokasi_perusahaan" value="<?php echo $iku_1['lokasi_perusahaan']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Pendapatan</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="pendapatan" value="<?php echo $iku_1['pendapatan']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Lanjut Studi</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="lanjut_studi" value="<?php echo $iku_1['lanjut_studi']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tautan Bukti</label>
+            <div class="col-sm-12 col-md-7">
+              <input type="text" class="form-control" name="tautan_bukti" value="<?php echo $iku_1['tautan_bukti']; ?>">
+            </div>
+          </div>
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Status Validasi</label>
+            <div class="col-sm-12 col-md-7">
+              <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="ya" name="status_validasi" class="custom-control-input" <?php if($iku_1['status_validasi'] == '1') echo 'checked' ?>>
+                <label class="custom-control-label" for="ya">Ya</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="tidak" name="status_validasi" class="custom-control-input" <?php if($iku_1['status_validasi'] == '0') echo 'checked' ?>>
+                <label class="custom-control-label" for="tidak">Tidak</label>
+              </div>
+            </div>
+          </div>
+
+          <div class="form-group row mb-4">
+            <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+            <div class="col-sm-12 col-md-7">
+              <button class="btn btn-primary">Update</button>
+              <a href="<?= base_url('iku1') ?>" class="btn btn-danger">Back</a>
+            </div>
+          </div>
+        </div>
+      </form>
+
+
+    </div>
+  </section>
 </div>
 
 
 
 
-<?php echo view('layout/footer')?>
+<?php echo view('layout/footer') ?>
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -8,16 +8,16 @@
             <!-- <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
             <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li> -->
           </ul>
-          
-                <!-- <div class="card">
+
+          <!-- <div class="card">
                   <div class="card-header">
                     <div class="form-control text center">
                         <h2 class="">IKU UNSIKA</h2>
                     </div>
                   </div>
                 </div> -->
-    
-            
+
+
           <!-- <div class="search-element">
             <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
             <button class="btn" type="submit"><i class="fas fa-search"></i></button>
@@ -31,20 +31,20 @@
         </form>
         <!-- <ul class="navbar-nav navbar-right">
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-            <img alt="image" src="<?php echo base_url ('assets/img/avatar/avatar-1.png')?>" class="rounded-circle mr-1">
+            <img alt="image" src="<?php echo base_url('assets/img/avatar/avatar-1.png') ?>" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block"><?= session()->get('nama') ?></div><br>
               <?php if (session()->get('role') == 1) {
-                        echo ' Mahasiswa';
-                    } else if (session()->get('role') == 2) {
-                        echo 'Pembimbing';
-                    } else {
-                        echo 'User';
-                    } ?>
+                echo ' Mahasiswa';
+              } else if (session()->get('role') == 2) {
+                echo 'Pembimbing';
+              } else {
+                echo 'User';
+              } ?>
             <div class="dropdown-menu dropdown-menu-right">
               <a href="features-profile.html" class="dropdown-item has-icon">
                 <i class="far fa-user"></i> Profile
               </a>
-              <a href="<?= base_url('Auth/logout')?>" class="dropdown-item has-icon text-danger">
+              <a href="<?= base_url('Auth/logout') ?>" class="dropdown-item has-icon text-danger">
                 <i class="fas fa-sign-out-alt"></i> Logout
               </a>
             </div>
