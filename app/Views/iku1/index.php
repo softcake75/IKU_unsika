0e9f54c9ab01ada4cf5581c4639cfdc5d9a3e867
Adam Mukti <adammukti08@gmail.com>
2021-11-25 11:01:22 +0700

    Fix iku1

--- app/Views/iku1/index.php
+++ app/Views/iku1/index.php
@@ -1,7 +1,20 @@
-<?php echo view('layout/header')?>
-<?php echo view('layout/navbar')?>
-<?php echo view('layout/sidebar')?>
+<?php echo view('layout/header') ?>
+<?php echo view('layout/navbar') ?>
+<?php echo view('layout/sidebar') ?>
 
+<div class="main-content">
+  <section class="section">
+    <div class="section-header">
+      <h1>IKU 1 (Lulusan)</h1>
+      <div class="section-header-breadcrumb">
+        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#add_iku1">Tambah data</button>
+      </div>
+    </div>
+    <div class="section-header">
+      <div class="form-group">
+        <h6>Total Jumlah Lulusan S1/D4/D3/D2 = <button class="card bg-danger"><?= $totalMhs ?></button></h6>
+      </div>
+    </div>
 
     <div class="card">
       <div class="card-header">
@@ -80,154 +93,156 @@
           </table>
         </div>
       </div>
-   
+    </div>
+  </section>
+</div>
 
-    <!-- Add Data -->
-    <form action="/iku1/save" method="post">
-        <div class="modal fade" id="add_iku1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Tambah data IKU 1</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                <label>Jenis</label>
-                    <select type="text" class="form-control" name="jenis"  placeholder="Masukan Jenis">
-                        <option value="Mendapat Pekerjaan">Mendapat Pekerjaan</option>
-                        <option value="Melanjutkan Studi">Melanjutkan Studi</option>
-                        <option value="Menjadi Wiraswasta">Menjadi Wiraswasta</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label>Nama</label>
-                    <input type="text" class="form-control" name="nama" placeholder="MAsukan Nama">
-                </div>
-                <div class="form-group">
-                    <label>NIM</label>
-                    <input type="number" class="form-control" name="nim" placeholder="MAsukan NIM">
-                </div>
-                <div class="form-group">
-                    <label>Jenjang</label>
-                    <input type="text" class="form-control" name="jenjang" placeholder="Masukan Jenjang">
-                </div>
-                <div class="form-group">
-                    <label>Prodi</label>
-                    <input type="text" class="form-control" name="prodi" placeholder="Masukan Prodi">
-                </div>
-                <div class="form-group">
-                    <label>Fakultas</label>
-                    <input type="text" class="form-control" name="fakultas" placeholder="Masukan Fakultas">
-                </div>
-                <div class="form-group">
-                    <label>No.Ijazah</label>
-                    <input type="text" class="form-control" name="no_ijazah" placeholder="Masukan No.Ijazah">
-                </div>
-                <div class="form-group">
-                    <label>Masa Tunggu</label>
-                    <input type="number" class="form-control" name="masa_tunggu" placeholder="Masukan Masa Tunggu">
-                </div>
-                <div class="form-group">
-                    <label>Nama Perusahaan</label>
-                    <input type="text" class="form-control" name="nama_perusahaan" placeholder="Masukan Nama Perusahaan">
-                </div>
-                <div class="form-group">
-                    <label>Lokasi Perusahaan</label>
-                    <input type="text" class="form-control" name="lokasi_perusahaan" placeholder="Masukan Lokasi Perusahaan">
-                </div>
-                <div class="form-group">
-                    <label>Pendapatan</label>
-                    <input type="text" class="form-control" name="pendapatan" placeholder="Masukan Pendapatan">
-                </div>
-                <div class="form-group">
-                    <label>Lanjut Studi</label>
-                    <input type="text" class="form-control" name="lanjut_studi" placeholder="Masukan Lanjut Studi">
-                </div>
-                <div class="form-group">
-                    <label>Tautan Bukti</label>
-                    <input type="text" class="form-control" name="tautan_bukti" placeholder="Masukan Tautan">
-                </div>
-                <div class="form-group">
-                    <label>Status Validasi</label><br>
-                    <!-- <input type="radio" class="form-control" name="status_validasi" value="1" placeholder="MAsukan Tanggal">/Yes -->
-                    <input type="checkbox" value="1" id="" name="status_validasi">
-                    <label for="status_validasi">Yes</label>
-                    <input type="checkbox" value="0" id="" name="status_validasi">
-                    <label for="status_validasi">NO</label>
-                </div>fta
-              </div>
-            </div>
+
+<!-- Add Data -->
+<form action="/iku1/save" method="post">
+  <div class="modal fade" id="add_iku1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Tambah data IKU 1</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="form-group">
+            <label>Jenis</label>
+            <select type="text" class="form-control" name="jenis" placeholder="Masukan Jenis">
+              <option value="Mendapat Pekerjaan">Mendapat Pekerjaan</option>
+              <option value="Melanjutkan Studi">Melanjutkan Studi</option>
+              <option value="Menjadi Wiraswasta">Menjadi Wiraswasta</option>
+            </select>
           </div>
+          <div class="form-group">
+            <label>Nama</label>
+            <input type="text" class="form-control" name="nama" placeholder="MAsukan Nama">
+          </div>
+          <div class="form-group">
+            <label>NIM</label>
+            <input type="number" class="form-control" name="nim" placeholder="MAsukan NIM">
+          </div>
+          <div class="form-group">
+            <label>Jenjang</label>
+            <input type="text" class="form-control" name="jenjang" placeholder="Masukan Jenjang">
+          </div>
+          <div class="form-group">
+            <label>Prodi</label>
+            <input type="text" class="form-control" name="prodi" placeholder="Masukan Prodi">
+          </div>
+          <div class="form-group">
+            <label>Fakultas</label>
+            <input type="text" class="form-control" name="fakultas" placeholder="Masukan Fakultas">
+          </div>
+          <div class="form-group">
+            <label>No.Ijazah</label>
+            <input type="text" class="form-control" name="no_ijazah" placeholder="Masukan No.Ijazah">
+          </div>
+          <div class="form-group">
+            <label>Masa Tunggu</label>
+            <input type="number" class="form-control" name="masa_tunggu" placeholder="Masukan Masa Tunggu">
+          </div>
+          <div class="form-group">
+            <label>Nama Perusahaan</label>
+            <input type="text" class="form-control" name="nama_perusahaan" placeholder="Masukan Nama Perusahaan">
+          </div>
+          <div class="form-group">
+            <label>Lokasi Perusahaan</label>
+            <input type="text" class="form-control" name="lokasi_perusahaan" placeholder="Masukan Lokasi Perusahaan">
+          </div>
+          <div class="form-group">
+            <label>Pendapatan</label>
+            <input type="text" class="form-control" name="pendapatan" placeholder="Masukan Pendapatan">
+          </div>
+          <div class="form-group">
+            <label>Lanjut Studi</label>
+            <input type="text" class="form-control" name="lanjut_studi" placeholder="Masukan Lanjut Studi">
+          </div>
+          <div class="form-group">
+            <label>Tautan Bukti</label>
+            <input type="text" class="form-control" name="tautan_bukti" placeholder="Masukan Tautan">
+          </div>
+          <div class="form-group">
+            <label>Status Validasi</label><br>
+            <!-- <input type="radio" class="form-control" name="status_validasi" value="1" placeholder="MAsukan Tanggal">/Yes -->
+            <input type="checkbox" value="1" id="" name="status_validasi">
+            <label for="status_validasi">Yes</label>
+            <input type="checkbox" value="0" id="" name="status_validasi">
+            <label for="status_validasi">NO</label>
+          </div>fta
         </div>
-        </div>
-    </form>
+      </div>
+    </div>
+  </div>
+  </div>
+</form>
 <!-- End Add data -->
 
-<?php echo view('layout/footer')?>
+<?php echo view('layout/footer') ?>
 
 <script>
-$(document).ready( function () {
+  $(document).ready(function() {
     $('#mydata').DataTable({
       responsive: {
         details: {
-                display: $.fn.dataTable.Responsive.display.modal( {
-                    header: function ( row ) {
-                        var data = row.data();
-                        return 'Details for '+data[0]+' '+data[2];
-                    }
-                } ),
-                renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
-                    tableClass: 'table'
-                } )
+          display: $.fn.dataTable.Responsive.display.modal({
+            header: function(row) {
+              var data = row.data();
+              return 'Details for ' + data[0] + ' ' + data[2];
             }
+          }),
+          renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+            tableClass: 'table'
+          })
+        }
       }
     });
-} );
+  });
 </script>
 
 
-<script type="text/javascript" language="javascript" >
-$(document).ready(function() {
-  var dataTable = $('#tabel_serverside').DataTable( {
-  "processing" : true,
-  "oLanguage": {
-                "sLengthMenu": "Tampilkan _MENU_ data per halaman",
-                "sSearch": "Pencarian: ",
-                "sZeroRecords": "Maaf, tidak ada data yang ditemukan",
-                "sInfo": "Menampilkan _START_ s/d _END_ dari _TOTAL_ data",
-                "sInfoEmpty": "Menampilkan 0 s/d 0 dari 0 data",
-                "sInfoFiltered": "(di filter dari _MAX_ total data)",
-                "oPaginate": {
-                    "sFirst": "<<",
-                    "sLast": ">>",
-                    "sPrevious": "<",
-                    "sNext": ">"
-               }
-            },
-  columnDefs: [{
-        targets: [0],
-        orderable: false
-     }],
-    "ordering": true,
-    "info": true,
-    "serverSide": true,
-    "stateSave" : true,
-  "scrollX": true,
-  "ajax":{
-          url :"<?php echo base_url("Utama/listdata"); ?>", // json datasource
-          type: "post",  // method  , by default get
-          error: function(){  // error handling
+<script type="text/javascript" language="javascript">
+  $(document).ready(function() {
+    var dataTable = $('#tabel_serverside').DataTable({
+        "processing": true,
+        "oLanguage": {
+          "sLengthMenu": "Tampilkan _MENU_ data per halaman",
+          "sSearch": "Pencarian: ",
+          "sZeroRecords": "Maaf, tidak ada data yang ditemukan",
+          "sInfo": "Menampilkan _START_ s/d _END_ dari _TOTAL_ data",
+          "sInfoEmpty": "Menampilkan 0 s/d 0 dari 0 data",
+          "sInfoFiltered": "(di filter dari _MAX_ total data)",
+          "oPaginate": {
+            "sFirst": "<<",
+            "sLast": ">>",
+            "sPrevious": "<",
+            "sNext": ">"
+          }
+        },
+        columnDefs: [{
+          targets: [0],
+          orderable: false
+        }],
+        "ordering": true,
+        "info": true,
+        "serverSide": true,
+        "stateSave": true,
+        "scrollX": true,
+        "ajax": {
+          url: "<?php echo base_url("Utama/listdata"); ?>", // json datasource
+          type: "post", // method  , by default get
+          error: function() { // error handling
             $(".tabel_serverside-error").html("");
             $("#tabel_serverside").append('<tbody class="tabel_serverside-error"><tr><th colspan="3">Data Tidak Ditemukan di Server</th></tr></tbody>');
-            $("#tabel_serverside_processing").css("display","none");
- 
+            $("#tabel_serverside_processing").css("display", "none");
+
           }
         }
 
       }
     });
   });
-
