65518a5805ec4f7819eedeab178a785387f77bcd
Adam Mukti <adammukti08@gmail.com>
2021-11-25 15:12:33 +0700

    Fix view for IKU1 - IKU3c

--- app/Views/iku1/index.php
+++ app/Views/iku1/index.php
@@ -93,7 +93,7 @@
                   <td class="text-center">
                     <div class="btn-group">
                       <a href="<?php echo base_url('/iku1/edit/' . $row['id']); ?>" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-                      <a href="<?php echo base_url('/iku1/delete/' . $row['id']); ?>" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus produk <?php echo $row['jenis']; ?> ini?')"><i class="fas fa-trash-alt"></i></a>
+                      <a href="<?php echo base_url('/iku1/delete/' . $row['id']); ?>" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus <?php echo $row['nama']; ?>?')"><i class="fas fa-trash-alt"></i></a>
                     </div>
                   </td>
                 </tr>
